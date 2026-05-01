ce562774b06890b1056f6d2b80766b78b551838c
Aman Kataria <amankataria1647@gmail.com>
2026-05-01 23:13:27 +0530

    fix correct form path

--- document_selector.js
+++ document_selector.js
@@ -5,30 +5,12 @@ document.addEventListener("DOMContentLoaded", function () {
     cards.forEach(card => {
         card.addEventListener("click", function () {
 
-            const type = this.getAttribute("data-doc-type");
+            const type = this.dataset.docType;
+            console.log("Clicked:", type);
 
-            navigateToForm(type);
+            // 🔥 For now, all go to same form
+            window.location.href = "forms/affidavit_form.html";
         });
     });
-
-    function navigateToForm(type) {
-
-        const routes = {
-            challan: "forms/traffic_challan.html",
-            affidavit: "forms/affidavit.html",
-            poa: "forms/power_of_attorney.html",
-            rent: "forms/rent_agreement.html",
-            income: "forms/income_declaration.html",
-            will: "forms/will.html"
-        };
-
-        const path = routes[type];
-
-        if (path) {
-            window.location.href = path;
-        } else {
-            alert("Document not found!");
-        }
-    }
 
 });
